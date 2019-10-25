7092017f35950bf5b038de5b36da2576e1b19a28
woojunshim <scholarticone@gmail.com>
2019-10-25 14:25:33 +1000

    Update

--- python/disc.py
+++ python/disc.py
@@ -49,10 +49,12 @@
 #   -o output file name (required)
 #   -p pseudo-count to be added to the input data, default = 1
 #   -l natural log-transformation of the input data, default = True    
+#   -r repressive tendency score file name, default = human_rts.txt
+#   -f filtering of non-priority genes, default = False
   
-#   E.g. >>> python disc.py -i example_input.txt -o example_file.txt 
+#   E.g. >>> python disc.py -i example_input.txt -o example_file.txt -f True
   
-#   --> Run TRIAGE on "example_input.txt" and output the discordance score as a text file "example_output.txt", with pseudo-count of 1 and natural log-transformation 
+#   --> Run TRIAGE on "example_input.txt" (-i example_input.txt) and output the discordance score as a text file "example_output.txt" (-o example_file.txt), with pseudo-count of 1 and natural log-transformation, using the human RTS table. Only output priority genes (-f True). 
 
 # If any issues are found, please contact to w.shim@uq.edu.au
 
@@ -76,16 +78,19 @@ def read_input(filename):
             results[t1[0]][t2] = float(t1[idx])
     return results
 
-def read_ref(filename):
+def read_ref(filename, col, numeric=True):
     """ read a repressive tendency score file """
     results = {}
     file_ = open(filename, 'r')
     for line in file_:
         line = line.strip().split()
-        results[line[0]] = float(line[1])
+        if numeric==True:
+            results[line[0]] = float(line[col])
+        else:
+            results[line[0]] = line[col]
     return results
 
-def perform_analysis(exp, ref, pseudo_=1, log_conversion=True):  
+def perform_analysis(exp, ref, pseudo_=1, log_conversion=True, priority=False):  
     """ main function to calcualte the discordance score """    
     if pseudo_!=None:
         exp = add_pseudo(exp, count_=pseudo_)    
@@ -93,18 +98,27 @@ def perform_analysis(exp, ref, pseudo_=1, log_conversion=True):
         for g in exp:
             for c in exp[g]:
                 exp[g][c] = np.log(exp[g][c])    
-    results = calculate_scores(exp, ref)    
+    results = calculate_scores(exp, ref, priority)    
     return results
 
 
-def calculate_scores(dic1, dic2):
+def calculate_scores(dic1, dic2, priority):
     """ calculate discordance scores, dic1 = expression table, dic2 = RTS table """
-    results = {}
+    results = {}   
     for g in dic1:
-        if g in dic2:  # only considers genes with RTS
-            results[g] = {}
-            for c in dic1[g]:
-                results[g][c] = dic1[g][c] * dic2[g]
+        if priority==True:            
+            if not g in priority_genes:                
+                continue
+            else:
+                if g in dic2:  # only considers genes with RTS                
+                    results[g] = {}
+                    for c in dic1[g]:
+                        results[g][c] = dic1[g][c] * dic2[g]
+        else:
+            if g in dic2:  # only considers genes with RTS                
+                results[g] = {}
+                for c in dic1[g]:
+                    results[g][c] = dic1[g][c] * dic2[g]
     return results
 
 def add_pseudo(input_data, count_):
@@ -142,19 +156,22 @@ if __name__ == '__main__':
     parser.add_option('-o','--o', dest='output_file', help='output filename')
     parser.add_option('-p', '--p', dest='pseudo', help='pseudo count', default=1)
     parser.add_option('-l', '--l', dest='log_transform', help='log-transformation (True or False)', default=True)
+    parser.add_option('-r', '--r', dest='ref_file', help='repressive tendency score (RTS) filename', default='human_rts.txt')
+    parser.add_option('-f', '--f', dest='priority_focus', help='filtering: whether to output ONLY priority genes', default=False)
 
     options = parser.parse_args()[0]  
 
-    if options.log_transform == 'False':
-        options.log_transform = False
-    else:
-        options.log_transform = True  
+    options.log_transform = bool(options.log_transform)
+    options.priority_focus = bool(options.priority_focus)
 
     if (options.input_file == None) or (options.output_file == None):
         sys.exit('Exiting: Input filename (-i) and output filename (-o) are required.')
     else:        
         exp = read_input(options.input_file)
-        ref = read_ref('human_rts.txt')    # Specify a RTS file to be read        
-        results = perform_analysis(exp, ref, pseudo_=options.pseudo, log_conversion=options.log_transform)        
+        ref = read_ref(options.ref_file, col=1)    # Specify a RTS file to be read
+        if options.priority_focus == True:
+            temp = read_ref(options.ref_file, col=2, numeric=False)
+            priority_genes = set([i for i in temp if temp[i]=='Y'])
+        results = perform_analysis(exp, ref, pseudo_=options.pseudo, log_conversion=options.log_transform, priority=options.priority_focus)   
         write_file(results, options.output_file)
         
